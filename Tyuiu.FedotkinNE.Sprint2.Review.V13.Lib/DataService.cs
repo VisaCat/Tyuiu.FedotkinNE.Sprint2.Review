7d8bc9486e52320fc8f6388d7a5b1a5b0fd1d4ba
Sir Nik <vikfed3717@gmail.com>
2024-12-21 16:45:58 +0300

    Add project files.

---
+++ Tyuiu.FedotkinNE.Sprint2.Review.V13.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FedotkinNE.Sprint2.Review.V13.Lib
+{
+    public class DataService
+    {
+        public bool Check(double x, double y)
+        {
+            double one = x;
+            double two = y;
+
+            if (((((Math.Pow(one, 2) + Math.Pow(two, 2)) <= 1) && Math.Sin(one) > 0) || ((one + two) >= (1 - one))) && (two <= 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                        
+        }
+    }
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Review.V13/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.FedotkinNE.Sprint2.Review.V13.Lib;
+namespace Tyuiu.FedotkinNE.Sprint2.Review.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            // Запрос координат точки у пользователя
+            Console.WriteLine("Введите координаты точки (X, Y):");
+
+            Console.Write("X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            bool res;
+            Console.WriteLine(res = ds.Check(x, y));
+        }
+    }
+}
